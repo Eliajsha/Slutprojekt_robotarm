816e49cfd68eb8612dd0aea0236bab7a133de229
Eliajsha <elias.svensson1@yahoo.se>
2025-04-22 10:22:50 +0200

    Mestadels klar
    Lagt till för kameran samt lagt in en dödzon för flexsensorerna

--- Slutprojekt/Slutprojekt.ino
+++ Slutprojekt/Slutprojekt.ino
@@ -7,82 +7,67 @@ Servo middleServo;
 int servoPin_thumb = 9;
 int servoPin_index = 7;
 int servoPin_middle = 6;
-
+int flexValue1;
+int flexValue2;
+int flexValue3;
+int prevThumb = 0;
+int prevIndex = 0;
+int prevMiddle = 0;
 
 const int thumbPin = A0;
 const int indexPin = A1;
 const int middlePin = A2;
-
-int flexValue1;
-int flexValue2;
-int flexValue3;
+const int buttonPin = 11;
+const int thresh = 20;
+ 
+bool which = true;
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
   thumbServo.attach(servoPin_thumb);
   indexServo.attach(servoPin_index);
   middleServo.attach(servoPin_middle);
-  Serial.begin(9600);
+  pinMode(buttonPin, INPUT_PULLUP);
 }
 
 void loop() {
-  // Läser av analoga värdet (0-1023)
-  flexValue1 = analogRead(thumbPin) - 30;
-  flexValue2 = analogRead(indexPin);
-  flexValue3 = analogRead(middlePin);
+  int raw1 = analogRead(thumbPin);
+  int raw2 = analogRead(indexPin);
+  int raw3 = analogRead(middlePin);
 
-  // Skriver ut värdet i Serial Monitor
-  Serial.print("Flexsensorvärde för tumme: ");
-  Serial.print(flexValue1);
-  Serial.print("  Flexsensorvärde för pekfinger: ");
-  Serial.print(flexValue2);
-  Serial.print("  Flexsensorvärde för långfinger: ");
-  Serial.println(flexValue3);
+  flexValue1 = applyDeadzone(raw1, prevThumb, thresh);
+  flexValue2 = applyDeadzone(raw2, prevIndex, thresh);
+  flexValue3 = applyDeadzone(raw3, prevMiddle, thresh);
+  
+  int buttonState = digitalRead(buttonPin);
 
 
-  // Kort fördröjning för att undvika överflödig data
- 
 
-  thumb();
-  index();
-  middle();
+  if (buttonState == HIGH) {
+    which = !which;
+    delay(500);
+  }
 
-
-  if (Serial.available() > 0) {
-    int command = Serial.parseInt();
-
-    if (command == 00) {
-      thumbServo.write(0);
-      indexServo.write(0);
-    }
-    else if (command == 0) {
-      thumbServo.write(90);
-      indexServo.write(0);
-      middleServo.write(0);      
-    }
-    else if (command == 1) {
-      thumbServo.write(0);
-      indexServo.write(90);
-      middleServo.write(0);
-    }
-    else if (command == 2) {
-      thumbServo.write(0);
-      indexServo.write(0);      
-      middleServo.write(90);      
-    }
+  if (which == true) {
+    camera();
+  }
+  else {
+    thumb();
+    index();
+    middle();
   }
 }
 
 
 void thumb() {
- int servoAngle_thumb = map(flexValue1, 800, 880, 0, 180);
+ int servoAngle_thumb = map(flexValue1, 650, 800, 0, 180);
   servoAngle_thumb = constrain(servoAngle_thumb, 0, 180);
 
   thumbServo.write(servoAngle_thumb);
 }
 
 void index() {
-  int servoAngle_index = map(flexValue2, 800, 980, 0, 180);
+  int servoAngle_index = map(flexValue2, 800, 1000, 0, 180);
   servoAngle_index = constrain(servoAngle_index, 0, 180);
 
   indexServo.write(servoAngle_index);
@@ -92,5 +77,66 @@ void middle() {
   int servoAngle_middle = map(flexValue3, 800, 930, 0, 180);
   servoAngle_middle = constrain(servoAngle_middle, 0, 180);
 
-  indexServo.write(servoAngle_middle);
+  middleServo.write(servoAngle_middle);
+}
+
+void camera () {
+   if (Serial.available() > 0) {
+    int command = Serial.parseInt();
+
+    if (command == 0) {
+      thumbServo.write(0);
+      indexServo.write(0);
+      middleServo.write(0);
+    }
+
+    else if (command == 1) {
+      thumbServo.write(90);
+      indexServo.write(0);
+      middleServo.write(0);      
+    }
+
+    else if (command == 2) {
+      thumbServo.write(0);
+      indexServo.write(90);
+      middleServo.write(0);
+    }
+
+    else if (command == 3) {
+      thumbServo.write(0);
+      indexServo.write(0);      
+      middleServo.write(90);      
+    }
+    
+    else if (command == 12) {
+      thumbServo.write(90);
+      indexServo.write(90);      
+      middleServo.write(0);  
+    }
+
+    else if (command == 123) {
+      thumbServo.write(90);
+      indexServo.write(90);      
+      middleServo.write(90);  
+    }
+
+    else if (command == 23) {
+      thumbServo.write(0);
+      indexServo.write(90);      
+      middleServo.write(90);  
+    }
+
+    else if (command == 13) {
+      thumbServo.write(90);
+      indexServo.write(0);      
+      middleServo.write(90);  
+    }
+  }
+}
+
+int applyDeadzone(int rawValue, int &prevValue, int threshold) {
+  if (abs(rawValue - prevValue) > threshold) {
+    prevValue = rawValue;
+  }
+  return prevValue;
 }
